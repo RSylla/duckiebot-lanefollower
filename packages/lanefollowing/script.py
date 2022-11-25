09d85634355dd199f682b1d73b72b0104dd30e78
Raivo Sulla <raivo.sylla@gmail.com>
2022-11-25 12:30:57 +0200

    Edited script.py and added venv

--- packages/lanefollowing/script.py
+++ packages/lanefollowing/script.py
@@ -1,2 +1,12 @@
-message = "Hellow World!"
+from smbus2 import SMBus
+import os
+
+
+message = f"Hello from {os.environ['VEHICLE_NAME']}"
 print(message)
+
+with SMBus(12) as bus:
+    bus.pec = 1  # Enable PEC
+    b = bus.read_byte_data(0x3e, 0x11)
+    print(b)
+
